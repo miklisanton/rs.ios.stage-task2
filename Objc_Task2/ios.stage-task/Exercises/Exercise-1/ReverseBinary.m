8f51601db8cdc0cbeaae787a3f362ec928969033
anton <antonmiklis@icloud.com>
2021-05-15 18:39:39 +0300

    feat: ex1 completed

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,20 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    NSMutableArray *binArr = [NSMutableArray new];
+    for (int i = 0; i < 8; i++) {
+        [binArr addObject:[NSNumber numberWithInt:0]];
+    }
+    int i = 0;
+    while (n) {
+        binArr[i] = [NSNumber numberWithInt:n % 2];
+        n /= 2;
+        i++;
+    }
+    int reversed = 0;
+    for (int i = 7; i >= 0; i--) {
+        reversed += [binArr[i] intValue] * pow(2, 7 - i);
+    }
+    NSLog(@"%d", reversed);
+    return reversed;
 }
